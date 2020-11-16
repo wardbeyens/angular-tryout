73d394e3e50f71914997e1ce3f59813972b1f919
Ward Beyens <45149049+wardbeyens@users.noreply.github.com>
2020-11-16 10:27:37 +0100

    show hide elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'rwapp';
+export class AppComponent implements OnInit {
+  title = 'facket';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit() {
+    this.userService.populate();
+  }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -12,35 +12,42 @@ import { User } from '../models/user.model';
 })
 export class UserService {
   private currentUserSubject = new BehaviorSubject<User>({} as User);
-  public currentUser = this.currentUserSubject.asObservable().pipe(distinctUntilChanged())
+  public currentUser = this.currentUserSubject.asObservable().pipe(distinctUntilChanged());
 
   private isAuthenticatedSubject = new ReplaySubject<Boolean>(1);
   public isAuthenticated = this.isAuthenticatedSubject.asObservable();
 
-  constructor(
-    private ApiService: ApiService,
-    private http: HttpClient
-  ){}
+  constructor(private ApiService: ApiService, private http: HttpClient) {}
 
-  setAuth(user: User){
-    this.currentUserSubject.next(user);
-    this.isAuthenticatedSubject.next(true)
+  populate() {
+    this.purgeAuth();
   }
 
-  attemptAuth(type, credentials) : Observable<User>{
-    const route = (type === 'login') ? '/login' : '';
-    return this.ApiService.post('/users' + route, {user: credentials})
-    .pipe(
-      map(data =>{
-        this.setAuth(data.user);
-        return data
-      })
-    )
+  setAuth(user: User) {
+    this.currentUserSubject.next(user);
+    this.isAuthenticatedSubject.next(true);
+  }
 
+  purgeAuth() {
+    // Remove JWT from localstorage
+    // this.jwtService.destroyToken();
+    // Set current user to an empty object
+    this.currentUserSubject.next({} as User);
+    // Set auth status to false
+    this.isAuthenticatedSubject.next(false);
+  }
+
+  attemptAuth(type, credentials): Observable<User> {
+    const route = type === 'login' ? '/login' : '';
+    return this.ApiService.post('/users' + route, { user: credentials }).pipe(
+      map((data) => {
+        this.setAuth(data.user);
+        return data;
+      })
+    );
   }
 
   getCurrentUser(): User {
     return this.currentUserSubject.value;
   }
-
 }
